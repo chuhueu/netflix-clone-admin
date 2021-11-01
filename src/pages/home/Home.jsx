b7f48c9c473682fdba7fd745d4a39cb20ddcf97b
Hueu123 <chuhueu1@gmail.com>
2021-11-01 14:02:53 +0700

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,10 @@
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
-import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import { useEffect, useMemo, useState } from "react";
-import axios from 'axios';
+import axios from '../../axios';
 export default function Home() {
   const MONTHS = useMemo(
     () => [
